e1f24a0447cdf2e2450c7ff30ec7bcb80a7d1c85
Radhi Fadlillah <m.radhi.f@gmail.com>
2017-05-16 20:03:14 +0700

    Separate SOAP request

--- device.go
+++ device.go
@@ -1,17 +1,8 @@
 package onvif
 
-import (
-	"bytes"
-	"io/ioutil"
-	"net/http"
-	"regexp"
-	"time"
-
-	"github.com/clbanning/mxj"
-)
-
-var httpClient = &http.Client{
-	Timeout: time.Second * 5,
+var deviceXMLNs = []string{
+	`xmlns:tds="http://www.onvif.org/ver10/device/wsdl"`,
+	`xmlns:tt="http://www.onvif.org/ver10/schema"`,
 }
 
 // Device contains data of ONVIF camera
@@ -25,42 +16,19 @@ type Device struct {
 
 // GetSystemDateAndTime fetch date and time from ONVIF camera
 func (device Device) GetSystemDateAndTime() (string, error) {
-	// Create request
-	request := `<?xml version="1.0" encoding="UTF-8"?>
-		<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope">
-			<s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-				<GetSystemDateAndTime xmlns="http://www.onvif.org/ver10/device/wsdl"/>
-			</s:Body>
-		</s:Envelope>`
-
-	request = regexp.MustCompile(`\>\s+\<`).ReplaceAllString(request, "><")
-	request = regexp.MustCompile(`\s+`).ReplaceAllString(request, " ")
-
-	// Create request
-	buffer := bytes.NewBuffer([]byte(request))
-	req, err := http.NewRequest("POST", device.XAddr, buffer)
-	req.Header.Set("Content-Type", "application/soap+xml")
-	req.Header.Set("Charset", "utf-8")
-
-	// Send request
-	resp, err := httpClient.Do(req)
-	if err != nil {
-		return "", err
+	// Create SOAP
+	soap := SOAP{
+		Body:  "<tds:GetSystemDateAndTime/>",
+		XMLNs: deviceXMLNs,
 	}
-	defer resp.Body.Close()
 
-	// Read response body
-	responseBody, err := ioutil.ReadAll(resp.Body)
+	// Send SOAP request
+	response, err := soap.SendRequest(device.XAddr)
 	if err != nil {
 		return "", err
 	}
 
-	// Parse XML to map
-	mapXML, err := mxj.NewMapXml(responseBody)
-	if err != nil {
-		return "", err
-	}
-
-	dateTime, _ := mapXML.ValueForPathString("Envelope.Body.GetSystemDateAndTimeResponse.SystemDateAndTime")
+	// Parse response
+	dateTime, _ := response.ValueForPathString("Envelope.Body.GetSystemDateAndTimeResponse.SystemDateAndTime")
 	return dateTime, nil
 }
